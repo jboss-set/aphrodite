e780e9550ce752e3900803bbf62bb4fdfe51bea4
Ryan Emerson <remerson@redhat.com>
2015-09-23 10:56:26 +0100

    UnsupportedOperationException added to methods that won't be implemented for a while.

--- src/main/java/org/jboss/set/aphrodite/issue/trackers/bugzilla/BugzillaIssueTracker.java
+++ src/main/java/org/jboss/set/aphrodite/issue/trackers/bugzilla/BugzillaIssueTracker.java
@@ -100,7 +100,7 @@ public class BugzillaIssueTracker implements IssueTrackerService {
 
     @Override
     public List<Issue> getIssuesAssociatedWith(Patch patch) {
-        return null;
+        throw new UnsupportedOperationException("Not yet implemented.");
     }
 
     @Override
