56f4a86c53f5a8bdb5aaf59c57bc4e1fada42e90
Chao Wang <chaowan@redhat.com>
2017-12-15 10:22:31 +0800

    fix typo

--- github/src/main/java/org/jboss/set/aphrodite/repository/services/github/GitHubWrapper.java
+++ github/src/main/java/org/jboss/set/aphrodite/repository/services/github/GitHubWrapper.java
@@ -80,7 +80,7 @@ class GitHubWrapper {
             boolean mergeable = false;
             if (pullRequest.getMergeable() == null) {
                 // workaround https://github.com/jboss-set/aphrodite/issues/150
-                Utils.logWarnMessage(LOG, "Can not retreive " + pullRequest.getHtmlUrl() + " mergeable value");
+                Utils.logWarnMessage(LOG, "Can not retrieve " + pullRequest.getHtmlUrl() + " mergeable value");
             } else {
                 mergeable = pullRequest.getMergeable();
             }
